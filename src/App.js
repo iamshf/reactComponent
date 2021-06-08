2b6836181b7e6e003e6dabe34a209b8ffe1afd5e
iamshf <haofengsheng@gmail.com>
2021-06-08 16:46:39 +0800

    add react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy, Suspense} from 'react';
+import {BrowserRouter as Router, NavLink, Switch, Route} from "react-router-dom";
+import Style from './App.module.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Table = lazy(() => import("./table/example.js"));
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <Suspense fallback="loading">
+                <Router>
+                    <div className={Style.App}>
+                        <nav>
+                            <NavLink to="/table">table</NavLink>
+                        </nav>
+                        <div>
+                            <Switch>
+                                <Route path="/table" component={Table} />
+                            </Switch>
+                        </div>
+                    </div>
+                </Router>
+            </Suspense>
+        );
+    }
 }
-
-export default App;
