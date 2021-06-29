22408a16390fa4ba4f85daa0e129d12f28b61d31
盛浩锋 <haofengsheng@gmail.com>
2021-06-29 19:42:28 +0800

    add event

--- src/MultiSelect/index.js
+++ src/MultiSelect/index.js
@@ -3,17 +3,21 @@ import React from 'react';
 import Style from "./index.module.css";
 
 export default class MultiIndex extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {show_option: false, is_focus: false};
+    }
     render() {
         return (
-            <div>
-                <input type="text" />
-                <ul>
+            <div className={Style.main} onMouseOver={this._toogleFocus.bind(this, true)} onMouseOut={this._toogleFocus.bind(this, false)}>
+                <input type="text" onClick={this._showOptions} />
+                <ul className={this.state.show_option ? "" : Style.hide}>
                     {
                         this.props.data.map(item => {
                             return (
                                 <li key={item.id}>
-                                    <input type="checkbox" />
-                                    <label htmlFor=""></label>
+                                    <input type="checkbox" id={`MultiSelect_ckb_${item.id}`} />
+                                    <label htmlFor={`MultiSelect_ckb_${item.id}`}>{item.name}</label>
                                 </li>
                             );
                         })
@@ -21,5 +25,25 @@ export default class MultiIndex extends React.Component {
                 </ul>
             </div>
         );
+    }
+    componentDidMount() {
+        document.body.addEventListener("click", this._handleClick);
+    }
+    componentWillUnmount() {
+        document.body.removeEventListener("click", this._handleClick);
+    }
+    _toogleFocus = (is_focus, e) => this.setState({is_focus: is_focus});
+    _showOptions = e => this.setState({show_option: true});
+
+    _hideOptions = e => {
+        console.log(e);
+        if(!e.currentTarget.contains(e.relatedTarget)) {
+            this.setState({show_option: false});
+        }
+    }
+    _handleClick = e => {
+        if(!this.state.is_focus) {
+            this.setState({show_option: false});
+        }
     }
 }
