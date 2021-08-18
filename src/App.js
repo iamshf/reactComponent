dd332a1ffafebdbdc32f202c5e045e622c4121f1
iamshf <haofengsheng@gmail.com>
2021-08-18 14:39:11 +0800

    增加弹窗示例

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Style from './App.module.css';
 
 const Table = lazy(() => import("./table/example.js"));
 const Recorder = lazy(() => import("./recorder/example.js"));
+const Dialog = lazy(() => import("./dialog/example.js"));
 
 export default class App extends React.Component {
     render() {
@@ -14,11 +15,13 @@ export default class App extends React.Component {
                         <nav>
                             <NavLink to="/table">table</NavLink>
                             <NavLink to="/recorder">recorder</NavLink>
+                            <NavLink to="/dialog">dialog</NavLink>
                         </nav>
                         <div>
                             <Switch>
                                 <Route path="/table" component={Table} />
                                 <Route path="/recorder" component={Recorder} />
+                                <Route path="/dialog" component={Dialog} />
                             </Switch>
                         </div>
                     </div>
